c16b01cafdd71aa2b7e75d63b29bb023622a1918
Kareem-Krayem <kareemkrayem.w@gmail.com>
2020-09-06 01:17:28 +0300

    COVID19_TRACKER

---
+++ COVID19_tracker/build/precache-manifest.a7ac6d77cb6a74dbbc240420f8c55e63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e4aad9de92e244dc5beef309d8ecef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70e52c871ef06bc5987c",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "720c3965dcc32b07e7f8",
+    "url": "/static/css/main.1a0300b7.chunk.css"
+  },
+  {
+    "revision": "70e52c871ef06bc5987c",
+    "url": "/static/js/2.ba8ee660.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.ba8ee660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720c3965dcc32b07e7f8",
+    "url": "/static/js/main.51c0337f.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # COVID19_tracker_react
+
+# First Step
+
+download and clone the project
+
+# Second Step
+
+you need to run the following commands:
+
+    cd COVID19_tracker
+
+    npm install
+
+    npm run start
+
+and you are ready to go
+
+# Thank you Enjoy !
